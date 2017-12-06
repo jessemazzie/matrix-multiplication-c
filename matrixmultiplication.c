89fed3e2b751c10931549542ba2aa28bc125d283
jesse <jesse.mazzie@agile5technologies.com>
2017-12-06 00:06:50 -0500

    Implemented error checking on number of arguments.

--- matrixmultiplication.c
+++ matrixmultiplication.c
@@ -3,28 +3,35 @@
 
 void getMatrixSize(int *rows, int *columns, char fileName[20]);
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     int rows = 0, columns = 0;
-    
+    if(argc <= 2) {
+        printf("At least two CSV files (supplied via command line argument) are required. Exiting...\n");
+        exit(0);
+    }
     printf("%i\n\n", argc);
-    printf("%s",argv[1]);
+    printf("%s", argv[1]);
     getMatrixSize(&rows, &columns, argv[1]);
-    printf("Hello world\n");
 }
 
-void getMatrixSize(int *rows, int *columns, char fileName[20])
-{
-    FILE *file = fopen(fileName, "r");
+void getMatrixSize(int *rows, int *columns, char fileName[20]) {
+    FILE *file = fopen(fileName, "r"); //Open filestream
     char csvCharacter;
-    if(file == NULL)
+    if (file == NULL) {
         printf("File does not exist.\n");
-    else
-        printf("Not null.\n");
-    do
-    {
-        csvCharacter = fgetc(file);
-        //Following line was used for debugging purposes
-        //printf("%c", csvCharacter);
-    } while (csvCharacter != EOF);
+    } else {
+        do {
+            csvCharacter = fgetc(file);
+
+            if (csvCharacter == ',')
+                columns++;
+            else if (csvCharacter == '\n')
+                rows++;
+
+            //Following line was used for debugging purposes
+            //printf("%c", csvCharacter);
+        } while (csvCharacter != EOF);
+    }
+
+    fclose(file); //Close filestream
 }
